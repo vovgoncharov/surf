87af5acdc8879b97d6585fc61272c4f7d5422ed1
Volodymyr Honcharov <51455879+vovgoncharov@users.noreply.github.com>
2020-08-05 15:53:55 +0300

    header__finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 $(function () {
-    $('.header__slider').slick({})
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow">',
+        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow">',
+        asNavFor: '.slider-dots'
+    })
+    $('.slider-dots').slick({
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider'
+    })
 })
