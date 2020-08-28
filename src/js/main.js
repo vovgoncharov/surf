8ce1de358cc1e8847abff93c5a0c158c78117a4b
Volodymyr Honcharov <51455879+vovgoncharov@users.noreply.github.com>
2020-08-28 11:30:45 +0300

    surf__commit__slide

--- src/js/main.js
+++ src/js/main.js
@@ -12,4 +12,11 @@ $(function () {
         slidesToScroll: 4,
         asNavFor: '.header__slider'
     })
+    $('.surf-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow">',
+        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow">'
+    })
 })
